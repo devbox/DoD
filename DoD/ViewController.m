b20e2cc2031f6e8547d42b6b252694ae5c5c61ec
David Rentsch <david.rentsch@me.com>
2012-01-30 23:08:13 +0100

    test commit local

--- DoD/ViewController.m
+++ DoD/ViewController.m
@@ -5,7 +5,7 @@
 //  Created by Tobias Friedenauer on 18.01.12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
-//das ist ein erster Kommentar von reen
+//das ist ein erster Kommentar von ree
 
 #import "ViewController.h"
 #import "AppDelegate.h"
